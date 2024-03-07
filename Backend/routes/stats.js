bce81f0c42d34c25718700061f2e8aeea9f4a298
redom13 <2105001@ugrad.cse.buet.ac.bd>
2024-03-07 22:34:20 +0600

    Alhamdulillah basic stats done

--- Backend/routes/stats.js
+++ Backend/routes/stats.js
@@ -3,20 +3,82 @@ const router = express.Router();
 const db = require("../db/db");
 
 router.get("/routes", async (req, res, next) => {
-    const  year  = req.query.year;
-    try {
-        const sql = `SELECT (SELECT STATION_NAME FROM STATIONS WHERE STATION_ID=R.FROM_ST) FROM_STATION,(SELECT STATION_NAME FROM STATIONS WHERE STATION_ID=R.TO_ST) TO_STATION,COUNT(*) PASSENGER_CT FROM RESERVATION R NATURAL JOIN BOOKED_SEATS B 
-        WHERE EXTRACT(YEAR FROM R.DATE_OF_JOURNEY)=:year
+  const criteria = req.query.criteria;
+  console.log("criteria", criteria);
+  try {
+    let sql;
+    let result;
+    if (criteria === "all time") {
+      sql = `SELECT (SELECT STATION_NAME FROM STATIONS WHERE STATION_ID=R.FROM_ST) FROM_STATION,(SELECT STATION_NAME FROM STATIONS WHERE STATION_ID=R.TO_ST) TO_STATION,COUNT(*) PASSENGER_CT FROM RESERVATION R NATURAL JOIN BOOKED_SEATS B 
         GROUP BY R.FROM_ST,R.TO_ST
         ORDER BY COUNT(*) DESC
         FETCH FIRST 5 ROWS ONLY`;
-        const result = await db.execute(sql, [year], db.options);
-        res.json(result.rows);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Server error");
-        next(err);
+      result = await db.execute(sql, [], db.options);
+    } else if (criteria === "today") {
+      sql = `SELECT (SELECT STATION_NAME FROM STATIONS WHERE STATION_ID=R.FROM_ST) FROM_STATION,(SELECT STATION_NAME FROM STATIONS WHERE STATION_ID=R.TO_ST) TO_STATION,COUNT(*) PASSENGER_CT FROM RESERVATION R NATURAL JOIN BOOKED_SEATS B 
+            WHERE R.DATE_OF_JOURNEY = CURRENT_DATE
+            GROUP BY R.FROM_ST,R.TO_ST
+        ORDER BY COUNT(*) DESC
+        FETCH FIRST 5 ROWS ONLY`;
+      result = await db.execute(sql, [], db.options);
+    } else if (criteria === "this week") {
+      sql = `SELECT (SELECT STATION_NAME FROM STATIONS WHERE STATION_ID=R.FROM_ST) FROM_STATION,(SELECT STATION_NAME FROM STATIONS WHERE STATION_ID=R.TO_ST) TO_STATION,COUNT(*) PASSENGER_CT FROM RESERVATION R NATURAL JOIN BOOKED_SEATS B 
+            WHERE R.DATE_OF_JOURNEY BETWEEN CURRENT_DATE-7 AND CURRENT_DATE
+            GROUP BY R.FROM_ST,R.TO_ST
+            ORDER BY COUNT(*) DESC
+        FETCH FIRST 5 ROWS ONLY`;
+      result = await db.execute(sql, [], db.options);
+    } else if (criteria === "this month") {
+      sql = `SELECT (SELECT STATION_NAME FROM STATIONS WHERE STATION_ID=R.FROM_ST) FROM_STATION,(SELECT STATION_NAME FROM STATIONS WHERE STATION_ID=R.TO_ST) TO_STATION,COUNT(*) PASSENGER_CT FROM RESERVATION R NATURAL JOIN BOOKED_SEATS B 
+            WHERE EXTRACT(MONTH FROM R.DATE_OF_JOURNEY) = EXTRACT(MONTH FROM CURRENT_DATE)
+            AND EXTRACT(YEAR FROM R.DATE_OF_JOURNEY) = EXTRACT(YEAR FROM CURRENT_DATE)
+            GROUP BY R.FROM_ST,R.TO_ST
+            ORDER BY COUNT(*) DESC
+        FETCH FIRST 5 ROWS ONLY`;
+      result = await db.execute(sql, [], db.options);
+    } else if (criteria === "this year") {
+      sql = `SELECT (SELECT STATION_NAME FROM STATIONS WHERE STATION_ID=R.FROM_ST) FROM_STATION,(SELECT STATION_NAME FROM STATIONS WHERE STATION_ID=R.TO_ST) TO_STATION,COUNT(*) PASSENGER_CT FROM RESERVATION R NATURAL JOIN BOOKED_SEATS B 
+            WHERE EXTRACT(YEAR FROM R.DATE_OF_JOURNEY) = EXTRACT(YEAR FROM CURRENT_DATE)
+            GROUP BY R.FROM_ST,R.TO_ST
+            ORDER BY COUNT(*) DESC
+        FETCH FIRST 5 ROWS ONLY`;
+      result = await db.execute(sql, [], db.options);
     }
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+    next(err);
+  }
+});
+
+router.get("/weekly", async (req, res, next) => {
+  try {
+    const weekly = await db.execute(
+      `SELECT 
+        TO_CHAR(R.DATE_OF_JOURNEY, 'D') AS DAY_OF_WEEK,
+        COUNT(*) AS RESERVATION_CT 
+    FROM RESERVATION R 
+    NATURAL JOIN BOOKED_SEATS B 
+    WHERE TO_CHAR(R.DATE_OF_JOURNEY, 'IW') = TO_CHAR(CURRENT_DATE, 'IW')
+    AND EXTRACT(YEAR FROM R.DATE_OF_JOURNEY) = EXTRACT(YEAR FROM CURRENT_DATE)
+    GROUP BY TO_CHAR(R.DATE_OF_JOURNEY, 'D')`,
+      [],
+      db.options
+    );
+    const allDays = Array.from({ length: 7 }, (_, i) => i + 1); // [1, 2, 3, 4, 5, 6, 7]
+
+    const weeklyWithAllDays = allDays.map((day) => {
+      const foundDay = weekly.rows.find((row) => row.DAY_OF_WEEK == day);
+      return foundDay ? foundDay : { DAY_OF_WEEK: day, RESERVATION_CT: 0 };
     });
+
+    res.json(weeklyWithAllDays);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+    next(err);
+  }
+});
 
 module.exports = router;
--- Frontend/src/Components/Statistics.tsx
+++ Frontend/src/Components/Statistics.tsx
@@ -8,9 +8,14 @@ import {
   Td,
   Select,
   Text,
+  Center,
 } from "@chakra-ui/react";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import {Bar} from 'react-chartjs-2';
+import Chart from 'chart.js/auto';
+import {CategoryScale} from 'chart.js'; 
+Chart.register(CategoryScale);
 
 type RouteStat = {
   FROM_STATION: string;
@@ -18,14 +23,21 @@ type RouteStat = {
   PASSENGER_CT: number;
 };
 
+type WeekStat = {
+    DAY_OF_WEEK: number;
+    RESERVATION_CT: number;
+  };
+
 const Statistics = () => {
   const [routes, setRoutes] = useState<RouteStat[]>([]);
-  const [year, setYear] = useState(new Date().getFullYear());
+  //const [year, setYear] = useState(new Date().getFullYear());
+  const [criteria, setCriteria] = useState("all time");
+  const [weekStats, setWeekStats] = useState<WeekStat[]>([]);
 
   const getRoutes = () => {
     try {
       axios
-        .get(`http://localhost:5000/api/v1/stats/routes`, { params: { year } })
+        .get(`http://localhost:5000/api/v1/stats/routes`, { params: { criteria } })
         .then((response) => {
           console.log(response.data);
           setRoutes(response.data);
@@ -35,35 +47,76 @@ const Statistics = () => {
     }
   };
 
+  const getWeekStats = () => {
+    try {
+      axios
+        .get(`http://localhost:5000/api/v1/stats/weekly`)
+        .then((response) => {
+          console.log(response.data);
+          setWeekStats(response.data);
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
     getRoutes();
-  }, [year]);
+  }, [criteria]);
 
-  const years = Array.from(
-    { length: 5 },
-    (_, i) => new Date().getFullYear() - i
-  );
+  useEffect(() => {
+    getWeekStats();
+  }, []);
 
-  return (
-    <VStack mt={4}>
-      <Select
-        placeholder="Select year"
-        onChange={(e) => setYear(Number(e.target.value))}
-        defaultValue={year}
-        w="40%"
-      >
-        {years.map((year, index) => (
-          <option key={index} value={year}>
-            {year}
-          </option>
-        ))}
-      </Select>
-      {routes.length === 0 ? (
-        <Text fontSize="xl">
-          <strong>No reservation in {year}</strong>
+//   const years = Array.from(
+//     { length: 5 },
+//     (_, i) => new Date().getFullYear() - i
+//   );
+const criteriaOptions = ["today", "this week", "this month", "this year", "all time"];
+
+const weekStatData = {
+    labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    datasets: [
+      {
+        label: '# of Reservations',
+        data: weekStats.map((stat) => stat.RESERVATION_CT),
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderWidth: 0.5,
+      },
+    ],
+  };
+
+  const options = {
+    maintainAspectRatio: false,
+    scales: {
+        yAxes:[ 
+        {
+            ticks:{
+            beginAtZero: true,
+            }
+        }
+    ],
+    legend: {
+        labels: {
+            fontSize: 15,
+        },
+    },
+    },
+  };
+
+return (
+    <Center>
+    <VStack mt={4} w="80%">
+        <Text fontSize="xl" fontWeight="bold" mt={4}>
+          Top Routes Statistics 
         </Text>
-      ) : (
-        <Table variant="simple">
+      <Select onChange={(e) => setCriteria(e.target.value)} defaultValue={criteria} w="40%">
+            {criteriaOptions.map((option, index) => (
+                <option key={index} value={option}>{option}</option>
+            ))}
+        </Select>
+        <Table variant="simple" borderWidth="1px" border="solid" borderColor="blue.500">
           <Thead>
             <Tr>
               <Th bg="blue.500" color="white">
@@ -87,8 +140,9 @@ const Statistics = () => {
             ))}
           </Tbody>
         </Table>
-      )}
+        <Bar data={weekStatData} key={Math.random()}/>
     </VStack>
+    </Center>
   );
 };
 
