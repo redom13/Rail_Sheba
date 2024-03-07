7800a0b9960ae69e2785637095791a6b9f192884
redom13 <2105001@ugrad.cse.buet.ac.bd>
2024-03-07 15:00:35 +0600

    Bug fixed

--- Backend/routes/stations.js
+++ Backend/routes/stations.js
@@ -2,19 +2,20 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db/db");
 
-// router.get("/", async (req, res, next) => {
-//     try {
-//         const stations = await db.execute(
-//         `SELECT STATION_NAME FROM STATIONS`,
-//         [],
-//         db.options
-//         );
-//         res.json(stations.rows);
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).send("Server error");
-//     }
-//     });
+router.get("/", async (req, res, next) => {
+    try {
+        const stations = await db.execute(
+        `SELECT STATION_NAME FROM STATIONS`,
+        [],
+        db.options
+        );
+        res.json(stations.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server error");
+        next(err);
+    }
+    });
 
 router.get("/:train_id",async (req, res, next) => {
     try{
